d86806f0791e92ce74689b639bfed9e464610054
JiachengGao <43893901+JiachengGao@users.noreply.github.com>
2018-11-15 18:35:53 -0500

    Add files via upload
    we have already achieved most of our goals, but still, need to be completed.

--- project.sql
+++ project.sql
@@ -11,18 +11,18 @@ QueryName VARCHAR(300),
 ReleaseDate DECIMAL,
 ReleaseYear INT,
 RequiredAge INT,
-DLCCount INT,
+DLC INT,
 Metacritic INT,
-MovieCount INT,
-PackageCount INT,
-RecommendationCount INT,
-ScreenshotCount INT,
-SteamSpyOwners INT,
-SteamSpyOwnersVariance INT,
-SteamSpyPlayersEstimate INT NOT NULL,
-SteamSpyPlayersEstimateVariance INT NOT NULL,
-AchievementCount INT NOT NULL,
-AchievementHighlightedCount INT NOT NULL,
+Movie INT,
+Package INT,
+Recommendation INT,
+Screenshot INT,
+SteamOwners INT,
+SteamOwnersVariance INT,
+PlayersEstimate INT NOT NULL,
+PlayersEstimateVariance INT NOT NULL,
+Achievement INT NOT NULL,
+AchievementHighlighted INT NOT NULL,
 PriceInitial Float NOT NULL,
 PriceFinal float NOT NULL,
 SupportedLanguage VARCHAR(500) NOT NULL,
@@ -34,8 +34,8 @@ INTO TABLE general
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES
-(QueryID,@QueryName,@ReleaseDate, @ReleaseYear,RequiredAge,DLCCount,Metacritic,MovieCount,PackageCount,RecommendationCount,ScreenshotCount,SteamspyOwners,SteamSpyOwnersVariance,
-SteamSpyPlayersEstimate,SteamSpyPlayersEstimateVariance,AchievementCount,AchievementHighlightedCount,PriceInitial,PriceFinal,SupportedLanguage)
+(QueryID,@QueryName,@ReleaseDate, @ReleaseYear,RequiredAge,DLC,Metacritic,Movie,Package,Recommendation,Screenshot,SteamOwners,SteamOwnersVariance,
+PlayersEstimate,PlayersEstimateVariance,Achievement,AchievementHighlighted,PriceInitial,PriceFinal,SupportedLanguage)
 set
 QueryName = nullif(@QueryName,''),
 ReleaseDate = nullif(@ReleaseDate,''),
@@ -46,8 +46,8 @@ SELECT * FROM general;
 ## Create table platform
 DROP TABLE IF EXISTS platform;
 CREATE TABLE platform(
-ResponseID INT NOT NULL,
-ResponseName VARCHAR(300) NOT NULL,
+ID INT NOT NULL,
+Name VARCHAR(300) NOT NULL,
 ControllerSupport VARCHAR(10) NOT NULL,
 IsFree VARCHAR(10),
 FreeVerAvail VARCHAR(10),
@@ -79,27 +79,27 @@ DROP TABLE IF EXISTS category;
 CREATE TABLE category(
 QueryID INT NOT NULL,
 QueryName VARCHAR(300),
-CategorySinglePlayer VARCHAR(50),
-CategoryMultiplayer VARCHAR(50),
-CategoryCoop VARCHAR(50),
-CategoryMMO VARCHAR(50),
-CategoryInAppPurchase VARCHAR(50),
-CategoryIncludeSrcSDK VARCHAR(50),
-CategoryIncludeLevelEditor VARCHAR(50),
-CategoryVRSupport VARCHAR(50),
-GenreIsNonGame VARCHAR(50),
-GenreIsIndie VARCHAR(50),
-GenreIsAction VARCHAR(50),
-GenreIsAdventure VARCHAR(50),
-GenreIsCasual VARCHAR(50),
-GenreIsStrategy VARCHAR(50),
-GenreIsRPG VARCHAR(50),
-GenreIsSimulation VARCHAR(50),
-GenreIsEarlyAccess VARCHAR(50),
-GenreIsFreeToPlay VARCHAR(50),
-GenreIsSports VARCHAR(50),
-GenreIsRacing VARCHAR(50),
-GenreIsMassivelyMultiplayer VARCHAR(50),
+SinglePlayer VARCHAR(50),
+Multiplayer VARCHAR(50),
+Coop VARCHAR(50),
+MMO VARCHAR(50),
+InAppPurchase VARCHAR(50),
+IncludeSrcSDK VARCHAR(50),
+IncludeLevelEditor VARCHAR(50),
+VRSupport VARCHAR(50),
+NonGame VARCHAR(50),
+Indie VARCHAR(50),
+Action VARCHAR(50),
+Adventure VARCHAR(50),
+Casual VARCHAR(50),
+Strategy VARCHAR(50),
+RPG VARCHAR(50),
+Simulation VARCHAR(50),
+EarlyAccess VARCHAR(50),
+FreeToPlay VARCHAR(50),
+Sports VARCHAR(50),
+Racing VARCHAR(50),
+MassivelyMultiplayer VARCHAR(50),
 PRIMARY KEY(QueryID)
 );
 
@@ -108,10 +108,147 @@ INTO TABLE category
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(QueryID,QueryName,CategorySinglePlayer,CategoryMultiplayer,CategoryCoop,CategoryMMO,CategoryInAppPurchase,CategoryIncludeSrcSDK,CategoryIncludeLevelEditor,
-CategoryVRSupport,GenreIsNonGame,GenreIsIndie,GenreIsAction,GenreIsAdventure,GenreIsCasual,GenreIsStrategy,GenreIsRPG,GenreIsSimulation,
-GenreIsEarlyAccess,GenreIsFreeToPlay,GenreIsSports,GenreIsRacing,GenreIsMassivelyMultiplayer);
+(QueryID,QueryName,SinglePlayer,Multiplayer,Coop,MMO,InAppPurchase,IncludeSrcSDK,IncludeLevelEditor,
+VRSupport,NonGame,Indie,Action,Adventure,Casual,Strategy,RPG,Simulation,
+EarlyAccess,FreeToPlay,Sports,Racing,MassivelyMultiplayer);
 SELECT * FROM category limit 10;
+
+##----------------------------------
+## change ture/false to exactly category name they are
+set sql_safe_updates=0;
+UPDATE category
+SET SinglePlayer = 'SinglePlayer'
+WHERE SinglePlayer = 'TRUE';
+UPDATE category
+SET SinglePlayer = ''
+WHERE SinglePlayer = 'FALSE';
+
+UPDATE category
+SET Multiplayer = 'Multiplayer'
+WHERE Multiplayer = 'TRUE';
+UPDATE category
+SET Multiplayer = ''
+WHERE Multiplayer = 'FALSE';
+
+UPDATE category
+SET Coop = 'Coop'
+WHERE Coop = 'TRUE';
+UPDATE category
+SET Coop = ''
+WHERE Coop = 'FALSE';
+
+UPDATE category
+SET MMO = 'MMO'
+WHERE MMO = 'TRUE';
+UPDATE category
+SET MMO = ''
+WHERE MMO = 'FALSE';
+
+UPDATE category
+SET VRSupport = 'VRSupport'
+WHERE VRSupport = 'TRUE';
+UPDATE category
+SET VRSupport = ''
+WHERE VRSupport = 'FALSE';
+
+UPDATE category
+SET Action = 'Action'
+WHERE Action = 'TRUE';
+UPDATE category
+SET Action = ''
+WHERE Action = 'FALSE';
+
+UPDATE category
+SET Adventure = 'Adventure'
+WHERE Adventure = 'TRUE';
+UPDATE category
+SET Adventure = ''
+WHERE Adventure = 'FALSE';
+
+UPDATE category
+SET Casual = 'Casual'
+WHERE Casual = 'TRUE';
+UPDATE category
+SET Casual = ''
+WHERE Casual = 'FALSE';
+
+UPDATE category
+SET Strategy = 'Strategy'
+WHERE Strategy = 'TRUE';
+UPDATE category
+SET Strategy = ''
+WHERE Strategy = 'FALSE';
+
+UPDATE category
+SET RPG = 'RPG'
+WHERE RPG = 'TRUE';
+UPDATE category
+SET RPG = ''
+WHERE RPG = 'FALSE';
+
+UPDATE category
+SET Simulation = 'Simulation'
+WHERE Simulation = 'TRUE';
+UPDATE category
+SET Simulation = ''
+WHERE Simulation = 'FALSE';
+
+UPDATE category
+SET EarlyAccess = 'EarlyAccess'
+WHERE EarlyAccess = 'TRUE';
+UPDATE category
+SET EarlyAccess = ''
+WHERE EarlyAccess = 'FALSE';
+
+UPDATE category
+SET FreeToPlay = 'FreeToPlay'
+WHERE FreeToPlay = 'TRUE';
+UPDATE category
+SET FreeToPlay = ''
+WHERE FreeToPlay = 'FALSE';
+
+UPDATE category
+SET Sports = 'Sports'
+WHERE Sports = 'TRUE';
+UPDATE category
+SET Sports = ''
+WHERE Sports = 'FALSE';
+
+UPDATE category
+SET Racing = 'Racing'
+WHERE Racing = 'TRUE';
+UPDATE category
+SET Racing = ''
+WHERE Racing = 'FALSE';
+
+## contact all category togehter with view
+DROP view IF EXISTS cate;
+CREATE view cate AS
+SELECT QueryID as ID, QueryName as Game,CONCAT(SinglePlayer," ",Multiplayer," ",coop," ",MMO," ",
+VRSupport," ",Action," ",Adventure," ",Casual," ",Strategy," ",RPG,
+Simulation," ",EarlyAccess," ",FreeToPlay," ",Sports," ",Racing) as category
+FROM category;
+SELECT * FROM cate;
+
+##-----------------------------------------------------
+## FIND most recommendation game with category
+SELECT general.QueryName, general.Recommendation, cate.category
+FROM general
+Inner JOIN cate
+ON general.QueryID = cate.ID 
+ORDER BY general.Recommendation DESC limit 20; 
+
+#--------------------------------------------------------------------
+##Find game free and paid
+SELECT COUNT(QueryName) as FreeGame
+FROM general
+WHERE PriceFinal = 0;
+
+##----------------------------------------------
+# compare free and paid game
+SELECT QueryName FROM general WHERE PriceFinal = 0
+UNION
+SELECT QueryID FROM general WHERE PriceFinal!=0;
 
 ##Create table duration
 DROP TABLE IF EXISTS duration;
@@ -142,3 +279,73 @@ From general
 INNER JOIN category
 ON general.QueryName = category.QueryName
 ORDER BY RecommendationCount DESC limit 15;
+
+##--------------------------------------------------------------------------------------------------------
+## Using metacritic to compare the quality of games to the number of owners
+## and the quality of free games to paid 
+DROP VIEW IF EXISTS FreeGame;
+CREATE VIEW FreeGame AS
+SELECT QueryName, metacritic, Recommendation, SteamOwners, PriceFinal
+FROM general
+WHERE PriceFinal = '0';
+SELECT QueryName,  Recommendation,PriceFinal
+FROM FreeGame limit 10;
+
+DROP VIEW IF EXISTS PaidGame;
+CREATE VIEW PaidGame AS
+SELECT QueryName, metacritic, Recommendation, SteamOwners, PriceFinal
+FROM general
+WHERE PriceFinal != '0';
+SELECT QueryName,  Recommendation,PriceFinal
+FROM PaidGame limit 10;
+
+SELECT * FROM PaidGame;
+SELECT * FROM FreeGame;
+SELECT FreeGame.QueryName,FreeGame.metacritic,FreeGame.Recommendation,PaidGame.QueryName
+FROM FreeGame
+INNER JOIN PaidGame;
+
+SELECT QueryName
+FROM PaidGame 
+UNION
+SELECT QueryName
+FROM FreeGame;
+ #----------------------------------------------------------------------
+ # game count by released year
+ SELECT COUNT(QueryID),ReleaseYear
+ FROM general
+ GROUP BY ReleaseYear
+ ORDER BY ReleaseYear DESC;
+ 
+ #----------------------------------------
+ # for the game The Witcher 3: Hunter
+ SELECT QueryName, Recommendation, PriceFinal
+ FROM general
+ INNER JOIN duration
+ ON general.QueryName = duration.GameName
+ WHERE general.QueryName like '%Witcher%';
+
+select * 
+from duration
+where GameName like 'The Witcher 3%'
+GROUP by UserID;
+
+#--------------------------------------------------
+# get the screemshot and supported language to judge worldwild game
+SELECT QueryName, Screenshot,Recommendation, PriceFinal,
+(LENGTH(SupportedLanguage) - LENGTH(REPLACE(SupportedLanguage, ' ', '')) + 1) 
+AS NumberOfSupportedLanguage, SupportedLanguage
+FROM general
+WHERE Recommendation > 10000 
+#ORDER BY ScreenShot DESC;
+ORDER BY NumberOfSupportedLanguage DESC;
+#-------------------------------------------------------------------------------
+# popular worldwild game with category 
+SELECT general.QueryName, general.Recommendation, general.PriceFinal,
+(LENGTH(general.SupportedLanguage) - LENGTH(REPLACE(general.SupportedLanguage, ' ', '')) + 1) 
+AS NumberOfSupportedLanguage, cate.category
+FROM general
+INNER JOIN cate
+ON general.QueryName = cate.Game
+WHERE general.Recommendation>10000
+ORDER BY general.Recommendation DESC;
